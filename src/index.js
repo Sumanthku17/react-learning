bbbd26fc61f1982fb495c8d700cb3ce6930549c4
sumanth <sumanthku17@gmail.com>
2021-04-24 11:34:12 +0530

    spread_operator [...arr1,2,4,...arr2] {...abj1,sec :'B',...obj2}

--- src/index.js
+++ src/index.js
@@ -65,16 +65,29 @@
 // console.log(vari); // arr.map funtion
 
 
-const sumanth =
-{
-  name:"nanu",
-  age:"",
-  class:""
-}
+// const sumanth =
+// {
+//   name:"nanu",
+//   age:"",
+//   class:""
+// }
 
-//  const nm = sumanth.name;
-// const ag = sumanth.age;
-// const cl = sumanth.class;
+// //  const nm = sumanth.name;
+// // const ag = sumanth.age;
+// // const cl = sumanth.class;
 
-const {name : nm, age : ag, class : cl}= sumanth; // this object destructuring i.e. instead of above 3 lines u can use this single line
-console.log(nm);
+// const {name : nm, age : ag, class : cl}= sumanth; // this object destructuring i.e. instead of above 3 lines u can use this single line
+// console.log(nm);
+
+
+const first = [1, 3, 6]
+const second = [2, 4, 5]
+const combined1 = first.concat(second);
+const combined2 = [...first,0,...second]; // spread operator[...arr1,2,4,...arr2]
+console.log(combined1);
+console.log(combined2);
+
+const first_obj = {name1 : 'my-name', age1 : 25};
+const sec_obj = {name : 'your-name', age : 20};
+const combi = {...first_obj,sec :'B',...sec_obj};// spread operator{...abj1,...obj2}
+console.log(combi);
